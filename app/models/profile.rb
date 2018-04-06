9eb66e3e3baa925931371c5725ac3e7da2096681
RenatoAmaral <srkimbo@gmail.com>
2018-04-06 16:42:38 +0000

    Fixing issue with database schema containing jungle stuff

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,8 @@
 class Profile < ApplicationRecord
-<<<<<<< HEAD
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-=======
->>>>>>> a2e6b6f34c9a10552e62315acd27b3d75a307624
+
+  ## TODO VALIDATE NAME
 end
