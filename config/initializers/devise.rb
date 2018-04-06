e36546da4c1ddd36e5671e59e96a855429add5c8
RenatoAmaral <srkimbo@gmail.com>
2018-04-06 23:03:40 +0000

    FB was commented out for future

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -280,6 +280,6 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :facebook, ENV['YOUR_FACEBOOK_ID'], ENV['YOUR_FACEBOOK_SECRET'], scope: 'email', info_fields: 'email, name'
+  # config.omniauth :facebook, ENV['YOUR_FACEBOOK_ID'], ENV['YOUR_FACEBOOK_SECRET'], scope: 'email', info_fields: 'email, name'
 
 end
