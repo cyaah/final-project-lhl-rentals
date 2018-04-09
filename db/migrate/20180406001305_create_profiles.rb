18e4dd40f23ea5055d352f22c4975f6263291cf9
Philip Louie Templonuevo <philiplouietemplonuevo@yahoo.ca>
2018-04-09 21:08:45 +0000

    HEAD reset

--- db/migrate/20180406001305_create_profiles.rb
+++ db/migrate/20180406001305_create_profiles.rb
@@ -8,7 +8,7 @@ class CreateProfiles < ActiveRecord::Migration[5.1]
       t.text :address
       ## Database authenticatable
       t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      # t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
