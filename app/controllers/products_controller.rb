45256b874ff2f5622258be032cc86f0314f6b1c7
cyaah <calvin.yah@gmail.com>
2018-04-13 15:31:02 -0400

    Added search feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -51,7 +51,12 @@ class ProductsController < ApplicationController
 	end
 
 	def search 
-		
+		if params[:search].blank?
+			redirect_to(root_path, alert: "Empty field!") and return
+		else
+			@parameter = params[:search].downcase
+			@results = Product.all.where("lower(name) LIKE :search", search: "%#{@parameter}%")
+		end					
 	end
 
 	 def product_params
