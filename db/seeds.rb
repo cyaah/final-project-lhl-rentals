f5fae203121c838c5a41d1d621fe078b05d4d55b
RenatoAmaral <srkimbo@gmail.com>
2018-04-18 15:11:11 +0000

    Small changes on styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,9 @@ puts "Finding or Creating Users"
 User.destroy_all
 
 User.create!([
-  {id: 1, fullname: "User One", phone:"555-555-5555", address: "301 Front St W, Toronto, ON M5V 2T6", email: "user1@email.com", password: "user1", password_confirmation: "user1", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
-  {id: 2, fullname: "User Two", phone:"555-444-6666", address: "6650 Niagara Parkway. Niagara Falls, ON L2G 0L0", email: "user2@email.com", password: "user2", password_confirmation: "user2", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:03:01", last_sign_in_at: "2015-02-06 14:03:01", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
-  {id: 3, fullname: "User Three", phone:"555-666-4444", address: "1 Austin Terrace, Toronto, ON M5R 1X8", email: "user3@email.com", password: "user3", password_confirmation: "user3", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:03:44", last_sign_in_at: "2015-02-06 14:03:44", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"}
+  {fullname: "User One", phone:"555-555-5555", address: "301 Front St W, Toronto, ON M5V 2T6", email: "user1@email.com", password: "user1", password_confirmation: "user1", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
+  {fullname: "User Two", phone:"555-444-6666", address: "6650 Niagara Parkway. Niagara Falls, ON L2G 0L0", email: "user2@email.com", password: "user2", password_confirmation: "user2", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:03:01", last_sign_in_at: "2015-02-06 14:03:01", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"},
+  {fullname: "User Three", phone:"555-666-4444", address: "1 Austin Terrace, Toronto, ON M5R 1X8", email: "user3@email.com", password: "user3", password_confirmation: "user3", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:03:44", last_sign_in_at: "2015-02-06 14:03:44", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"}
 ])
 
 
@@ -29,10 +29,10 @@ puts "Finding or Creating Categories"
 Category.destroy_all
 
 #Categories
-cat1 = Category.find_or_create_by! name: 'Tools'
-cat2 = Category.find_or_create_by! name: 'Sports Equipment'
-cat3 = Category.find_or_create_by! name: 'Electronics'
-cat4 = Category.find_or_create_by! name: 'Furniture'
+cat1 = Category.find_or_create_by! name: 'Electronics'
+cat2 = Category.find_or_create_by! name: 'Furniture'
+cat3 = Category.find_or_create_by! name: 'Sports Equipment'
+cat4 = Category.find_or_create_by! name: 'Tools'
 
 
 
@@ -127,18 +127,6 @@ end
           end_availability: DateTime.new(2018, 06, 01)
           })
 
-
-
-# # REVIEWS
-#
-# 10.times do |index|
-#   Product.review.create! ({
-#     product_id: rand(1...24),
-#     profile_id: rand(1...3),
-#     description: Faker::Lorem.sentence(10, false, 0),
-#     rating: rand(1...5)
-#     })
-# end
 
 
 
